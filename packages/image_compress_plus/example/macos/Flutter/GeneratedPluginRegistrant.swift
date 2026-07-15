b442f9c860f91269fbfce5a0f81883594d732940
nailao <mr.wutangnailao@gmail.com>
2026-07-15 16:39:22 +0800

    chore(example): refresh generated plugin registrants

--- packages/image_compress_plus/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/image_compress_plus/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import image_compress_plus_macos
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
